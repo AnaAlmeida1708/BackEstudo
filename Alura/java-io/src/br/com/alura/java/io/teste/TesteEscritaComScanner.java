1a9097a39e320a635786086a1cefe8421bd53625
AnaAlmeida1708 <anacarolinama@gmail.com>
2020-03-17 08:17:21 -0300

    Parseando arquivo e Formatação de valores

--- Alura/java-io/src/br/com/alura/java/io/teste/TesteEscritaComScanner.java
+++ Alura/java-io/src/br/com/alura/java/io/teste/TesteEscritaComScanner.java
@@ -1,6 +1,8 @@
 package br.com.alura.java.io.teste;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TesteEscritaComScanner {
@@ -11,7 +13,30 @@ public class TesteEscritaComScanner {
 
 		while (scanner.hasNextLine()) {
 			String linha = scanner.nextLine();
-			System.out.println(linha);
+//			System.out.println(linha);
+			
+//			String[] valores = linha.split(",");
+//			System.out.println(Arrays.toString(valores));
+			
+			Scanner linhaScanner = new Scanner(linha);
+			linhaScanner.useLocale(Locale.US);//necessário usar para evitar erro pelo tipo de parse no tipo Double
+			linhaScanner.useDelimiter(",");
+			
+			String tipoConta = linhaScanner.next();
+			int agencia = linhaScanner.nextInt();
+			int numero = linhaScanner.nextInt();
+			String titular = linhaScanner.next();
+			double saldo = linhaScanner.nextDouble();
+			
+//			System.out.println(valor1 + valor2 + valor3 + valor4 + valor5);
+			
+			String valorFormatado = String.format(new Locale("pt_BR"),"%s %04d %08d %s %.2f", tipoConta, agencia, numero, titular, saldo);
+			//%s String %d inteiro %f float - para mais informações olhar documentação Formating numeric print
+//			System.out.println(valorFormatado);
+			
+			System.out.format("%s %04d %08d %s %.2f %n", tipoConta, agencia, numero, titular, saldo);
+			
+			linhaScanner.close();
 		}
 
 		scanner.close();
