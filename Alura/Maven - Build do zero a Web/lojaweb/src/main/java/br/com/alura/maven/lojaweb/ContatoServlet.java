288b5d6263a5ef0c8a86dc872ef21489e2818f5e
anaalmeida1708 <anacarolinama@gmail.com>
2020-05-19 09:02:23 -0300

    Criando um projeto Web - Maven

---
+++ Alura/Maven - Build do zero a Web/lojaweb/src/main/java/br/com/alura/maven/lojaweb/ContatoServlet.java
@@ -0,0 +1,25 @@
+package br.com.alura.maven.lojaweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(urlPatterns={"/contato"})
+public class ContatoServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter writer = response.getWriter();
+        writer.println("<html><h2>Entre em contato</h2></html>");
+        writer.close();
+	}
+
+
+}
