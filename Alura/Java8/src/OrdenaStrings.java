b86228242e7770793afef932a1009a5b788ebd09
anaalmeida1708 <anacarolinama@gmail.com>
2020-12-12 08:05:07 -0300

    Trabalhando com lambdas

--- Alura/Java8/src/OrdenaStrings.java
+++ Alura/Java8/src/OrdenaStrings.java
@@ -1,52 +1,48 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class OrdenaStrings {
-	
+
 	public static void main(String[] args) {
+		
 		List<String> palavras = new ArrayList<>();
 		palavras.addAll(Arrays.asList("alura online", "editora casa do código", "caelum"));
 		
-//		Collections.sort(palavras); usando a classe comparable para ordenar
-		Comparator<String> comparador = new ComparadorPorTamanho();
-//		Collections.sort(palavras, comparador); usando a classe comparator para ordenar
-		
-		palavras.sort(comparador);
+		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+		palavras.sort((s1, s2) -> s1.length() - s2.length());//outra opção omitindo o método compare
 		System.out.println(palavras);
+
+
+//		palavras.forEach(new Consumer<String>() {
+//			@Override
+//			public void accept(String s) {
+//				System.out.println(s);
+//			}
+//		});
+
+
+//		Consumer<String> impressor = s -> System.out.println(s);
+//		palavras.forEach(impressor);
+
 		
-		//iterando antes do Java 8
-//		for(String palavra: palavras) {
-//			System.out.println(palavra);
-//		}
+		// mesmo coisa que os códigos acima, mas usando lambda
+		palavras.forEach(s -> System.out.println(s));
+
 		
-		Consumer<String> consumidor = new ImprimiNaLinha();
-		palavras.forEach(consumidor);
+//		new Thread(new Runnable() {
+//
+//		    @Override
+//		    public void run() {
+//		        System.out.println("Executando um Runnable");
+//		    }
+//
+//		}).start();
 		
-	}
+		// mesmo coisa do código acima, mas usando lambda
+		new Thread(()-> System.out.println("Executando um Runnable")).start();
 
-}
-
-class ImprimiNaLinha implements Consumer<String>{
-	@Override
-	public void accept(String s) {
-		System.out.println(s);
-	}
-}
-
-class ComparadorPorTamanho implements Comparator<String> {
-
-	@Override
-	public int compare(String s1, String s2) {
-		if(s1.length() < s2.length()) {
-			return -1;
-		}
-		if(s1.length() > s2.length()) {
-			return 1;
-		}
-		return 0;
 	}
 	
+
 }
