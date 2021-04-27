5ab634315216b9ac4cf23ad5c99fca9a0d95be10
anaalmeida1708 <anacarolinama@gmail.com>
2021-04-27 07:11:37 -0300

    Integrando bibliotecas

--- Alura/Java Brasil - Formatação de datas, cpf e números nacionais/src/main/java/br/com/alura/validacao/Dinheiro.java
+++ Alura/Java Brasil - Formatação de datas, cpf e números nacionais/src/main/java/br/com/alura/validacao/Dinheiro.java
@@ -3,6 +3,7 @@ package br.com.alura.validacao;
 import javax.money.CurrencyUnit;
 import javax.money.Monetary;
 import javax.money.MonetaryAmount;
+import javax.money.NumberValue;
 import javax.money.convert.CurrencyConversion;
 import javax.money.convert.ExchangeRateProvider;
 import javax.money.convert.MonetaryConversions;
@@ -10,6 +11,10 @@ import javax.money.convert.MonetaryConversions;
 import org.javamoney.moneta.FastMoney;
 import org.javamoney.moneta.Money;
 import org.javamoney.moneta.convert.ExchangeRateType;
+import org.javamoney.moneta.function.MonetaryOperators;
+
+import br.com.caelum.stella.inwords.FormatoDeReal;
+import br.com.caelum.stella.inwords.NumericToWordsConverter;
 
 public class Dinheiro {
 	public static void main(String[] args) {
@@ -17,9 +22,13 @@ public class Dinheiro {
 		CurrencyUnit real = Monetary.getCurrency("BRL");
 		CurrencyUnit dolar = Monetary.getCurrency("USD");
 		MonetaryAmount valorParcela = Money.of(75, real);
-		System.out.println(valorParcela);
+		MonetaryAmount valorDesconto = valorParcela.multiply(10).with(MonetaryOperators.percent(10));
+		NumberValue valor = valorDesconto.getNumber();
+		NumericToWordsConverter conversor = new NumericToWordsConverter(new FormatoDeReal());
 		
+		System.out.println(valorParcela);
 		System.out.println("Total : " + valorParcela.multiply(10));
+		System.out.println(conversor.toWords(valor.doubleValue()));
 		
 		
 		/*
@@ -36,7 +45,6 @@ public class Dinheiro {
 		MonetaryAmount valorFinalProdutoDolar = valorProduto.add(valorImpostoDolar);
 		
 		System.out.println(valorFinalProdutoDolar);
-		
 		
 	}
 }
