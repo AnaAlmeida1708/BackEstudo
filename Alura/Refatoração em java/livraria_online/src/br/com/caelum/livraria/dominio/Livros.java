c0ff2984f7c187845060a5fed95ef9e6cbb47b48
anaalmeida1708 <anacarolinama@gmail.com>
2021-05-04 09:31:16 -0300

    Ocultar delegação e Introduzir método externo

--- Alura/Refatoração em java/livraria_online/src/br/com/caelum/livraria/dominio/Livros.java
+++ Alura/Refatoração em java/livraria_online/src/br/com/caelum/livraria/dominio/Livros.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.javamoney.moneta.Money;
 
