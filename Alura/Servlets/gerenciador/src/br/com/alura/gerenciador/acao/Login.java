c53f6a81db4c4eb6f0755bcaa2e688f1121ba6d1
anaalmeida1708 <anacarolinama@gmail.com>
2021-05-15 11:04:58 -0300

    Formulário de Login

---
+++ Alura/Servlets/gerenciador/src/br/com/alura/gerenciador/acao/Login.java
@@ -0,0 +1,33 @@
+package br.com.alura.gerenciador.acao;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.alura.gerenciador.model.Banco;
+import br.com.alura.gerenciador.model.Usuario;
+
+public class Login implements Acao {
+
+	@Override
+	public String executa(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		String login = request.getParameter("login");
+		String senha = request.getParameter("senha");
+		
+		System.out.println("Logando " + login);
+		
+		Banco banco = new Banco();
+		Usuario usuario = banco.existeUsuario(senha, login);
+		if(usuario != null) {
+			return "redirect:empresa?acao=ListaEmpresas";
+		} else {
+			return "redirect:empresa?acao=LoginForm";
+		}
+		
+	}
+
+}
--- Alura/Servlets/gerenciador/src/br/com/alura/gerenciador/model/Banco.java
+++ Alura/Servlets/gerenciador/src/br/com/alura/gerenciador/model/Banco.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class Banco {
 	
 	private static List<Empresa> empresas = new ArrayList<Empresa>();
+	private static List<Usuario> listaUsuarios= new ArrayList<>();
 	
 	static {
 		Empresa empresa1 = new Empresa("Alura", "03/05/2003");
@@ -16,6 +17,17 @@ public class Banco {
 		Banco.empresas.add(empresa1);
 		Banco.empresas.add(empresa2);
 		Banco.empresas.add(empresa3);
+		
+		Usuario u1 = new Usuario();
+	    u1.setLogin("Ana");
+	    u1.setSenha("1234");
+
+	    Usuario u2 = new Usuario();
+	    u2.setLogin("Higor");
+	    u2.setSenha("4321");
+
+	    listaUsuarios.add(u1);
+	    listaUsuarios.add(u2);
 		
 	}
 	
@@ -52,6 +64,15 @@ public class Banco {
 		Empresa empresa = buscaEmpresaPorId(id);
 		empresa.setNome(nome);
 		empresa.setDataAbertura(dataAbertura);
+	}
+	
+	public Usuario existeUsuario(String senha, String login) {
+		for (Usuario usuario : listaUsuarios) {
+			if(usuario.ehIgual(login, senha)) {
+				return usuario;
+			}
+		}
+		return null;
 	}
 
 }
