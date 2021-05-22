fb4f03fb9b990b742588d7378b07c236e58e16a4
anaalmeida1708 <anacarolinama@gmail.com>
2021-05-22 08:32:42 -0300

    Controle de transação - Java e JDBC

--- Alura/Java e JDBC/loja_virtual_repository/src/br/com/alura/loja_virtual_repository/dao/TestaCadastro.java
+++ Alura/Java e JDBC/loja_virtual_repository/src/br/com/alura/loja_virtual_repository/dao/TestaCadastro.java
@@ -10,27 +10,41 @@ public class TestaCadastro {
 
 	public static void main(String[] args) throws SQLException {
 		ConnectionFactory connectionFactory = new ConnectionFactory();
-		Connection connection = connectionFactory.recuperaConexao();
-		
-		String nome = "TECLADO";
-		String descricao = "TECLADO SEM FIO";
-		
-		PreparedStatement stm = connection.prepareStatement(" INSERT INTO PRODUTO (NOME,DESCRICAO) VALUES (?, ?)" , Statement.RETURN_GENERATED_KEYS);
-		
+		try(Connection connection = connectionFactory.recuperaConexao()){//Desta forma como a classe em questão implementa AutoCloseable o recurso será fechado automaticamente, independente se der erro
+			connection.setAutoCommit(false);
+
+			try(PreparedStatement stm = connection.prepareStatement(" INSERT INTO PRODUTO (NOME,DESCRICAO) VALUES (?, ?)" , Statement.RETURN_GENERATED_KEYS)) {
+
+				adicionarProduto("ROTEADOR", "ROTEADOR", stm);
+				adicionarProduto("SSD", "SSD 1T", stm);
+
+				connection.commit();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("Fazendo Rollback");
+				connection.rollback();
+			} 
+
+		}
+	}
+
+	private static void adicionarProduto(String nome, String descricao, PreparedStatement stm) throws SQLException {
+		//		if(nome.equals("SSD")) {
+		//			throw new RuntimeException("Erro ao cadastrar produto " + nome);
+		//		}
+
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
-		
-		stm.execute();
-		 ResultSet rs = stm.getGeneratedKeys();
-		
-		 while (rs.next()) {
-			Integer id = rs.getInt(1);
-			System.out.println(id);
-		}
-		 
-		 
-		connection.close();
 
+		stm.execute();
+		try(ResultSet rs = stm.getGeneratedKeys()){
+
+			while (rs.next()) {
+				Integer id = rs.getInt(1);
+				System.out.println(id);
+			}
+		}
 	}
 
 }
