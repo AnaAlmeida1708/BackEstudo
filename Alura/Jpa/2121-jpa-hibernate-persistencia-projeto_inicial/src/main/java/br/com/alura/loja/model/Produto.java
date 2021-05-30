9ff6def96260d4924fe1ea48868797109840fc7e
anaalmeida1708 <anacarolinama@gmail.com>
2021-05-30 09:53:38 -0300

    Ciclo de vida entidades

--- Alura/Jpa/2121-jpa-hibernate-persistencia-projeto_inicial/src/main/java/br/com/alura/loja/model/Produto.java
+++ Alura/Jpa/2121-jpa-hibernate-persistencia-projeto_inicial/src/main/java/br/com/alura/loja/model/Produto.java
@@ -4,11 +4,10 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -23,8 +22,11 @@ public class Produto {
 	private BigDecimal preco;
 	private LocalDate dataCadastro = LocalDate.now();
 	
-	@Enumerated(EnumType.STRING)
+	@ManyToOne //Muitos produtos estão vinculados a uma categoria
 	private Categoria categoria;
+	
+	public Produto() {
+	}
 	
 	public Produto(String nome, String descricao, BigDecimal preco, Categoria categoria) {
 		super();
--- Alura/Jpa/2121-jpa-hibernate-persistencia-projeto_inicial/src/main/java/br/com/alura/loja/teste/CadastroDeProduto.java
+++ Alura/Jpa/2121-jpa-hibernate-persistencia-projeto_inicial/src/main/java/br/com/alura/loja/teste/CadastroDeProduto.java
@@ -1,27 +1,29 @@
 package br.com.alura.loja.teste;
 
-import java.math.BigDecimal;
-
 import javax.persistence.EntityManager;
 
-import br.com.alura.loja.dao.ProdutoDAO;
 import br.com.alura.loja.model.Categoria;
-import br.com.alura.loja.model.Produto;
 import br.com.alura.loja.util.JpaUtil;
 
 public class CadastroDeProduto {
 
 	public static void main(String[] args) {
-		Produto celular = new Produto("nokia","legal", new BigDecimal("500"), Categoria.CELULARES);
-		
 		EntityManager em = new JpaUtil().getEntityManager();
 		
-		ProdutoDAO dao = new ProdutoDAO(em);
+		Categoria celulares = new Categoria("CELULARES");
 		
 		em.getTransaction().begin();
-		dao.cadastrar(celular);
-		em.getTransaction().commit();
-		em.close();
+		em.persist(celulares);
+		celulares.setNome("XPTO");
+		em.flush();
+		em.clear();
+		
+		celulares = em.merge(celulares);
+		celulares.setNome("1234");
+		em.flush();
+		
+		em.remove(celulares);
+		em.flush();
 
 	}
 
