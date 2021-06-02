b388308833e9d2bdeab2a43f884c1f86fde2ef67
anaalmeida1708 <anacarolinama@gmail.com>
2021-06-02 10:31:42 -0300

    Consultas avançadas, uso Select new

--- Alura/Jpa/2121-jpa-hibernate-persistencia-projeto_inicial/src/main/java/br/com/alura/loja/dao/PedidoDAO.java
+++ Alura/Jpa/2121-jpa-hibernate-persistencia-projeto_inicial/src/main/java/br/com/alura/loja/dao/PedidoDAO.java
@@ -1,8 +1,12 @@
 package br.com.alura.loja.dao;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import br.com.alura.loja.model.Pedido;
+import br.com.alura.loja.model.RelatorioDeVendasVo;
 
 public class PedidoDAO {
 	
@@ -16,9 +20,26 @@ public class PedidoDAO {
 		this.em.persist(pedido);
 	}
 	
-	
 	public Pedido buscarPorId(Long id) {
 		return this.em.find(Pedido.class, id);
 	}
+	
+	public BigDecimal somaValorTotalPedidos() {
+		String jpql = "SELECT SUM(p.valorTotal) FROM Pedido p";
+		return em.createQuery(jpql, BigDecimal.class)
+				.getSingleResult();
+	}
+	
+	public List<RelatorioDeVendasVo> getRelatorioVendas(){
+		String jpql = "SELECT new br.com.alura.loja.model.RelatorioDeVendasVo (produto.nome, SUM(item.quantidade), MAX(pedido.data)) "
+				+ " FROM Pedido pedido "
+				+ " JOIN pedido.itens item "
+				+ " JOIN item.produto produto "
+				+ " GROUP BY produto.nome, item.quantidade,pedido.data  "
+				+ " ORDER BY item.quantidade DESC";
+		return em.createQuery(jpql, RelatorioDeVendasVo.class)
+				.getResultList();
+	}
+	
 	
 }
--- Alura/Jpa/2121-jpa-hibernate-persistencia-projeto_inicial/src/main/java/br/com/alura/loja/teste/CadastroPedidoCliente.java
+++ Alura/Jpa/2121-jpa-hibernate-persistencia-projeto_inicial/src/main/java/br/com/alura/loja/teste/CadastroPedidoCliente.java
@@ -1,5 +1,8 @@
 package br.com.alura.loja.teste;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import br.com.alura.loja.dao.ClienteDAO;
@@ -9,6 +12,7 @@ import br.com.alura.loja.model.Cliente;
 import br.com.alura.loja.model.ItemPedido;
 import br.com.alura.loja.model.Pedido;
 import br.com.alura.loja.model.Produto;
+import br.com.alura.loja.model.RelatorioDeVendasVo;
 import br.com.alura.loja.util.JpaUtil;
 
 public class CadastroPedidoCliente {
@@ -17,22 +21,34 @@ public class CadastroPedidoCliente {
 		EntityManager em = new JpaUtil().getEntityManager();
 		
 		ProdutoDAO produtoDAO = new ProdutoDAO(em);
-		Produto produto = produtoDAO.buscarPorId(1l);
+		Produto produto1 = produtoDAO.buscarPorId(1l);
+		Produto produto2 = produtoDAO.buscarPorId(2l);
+		Produto produto3 = produtoDAO.buscarPorId(3l);
 		
 		ClienteDAO clienteDAO = new ClienteDAO(em);
 		Cliente cliente = clienteDAO.buscarPorId(1l);
 		
 		PedidoDAO pedidoDAO = new PedidoDAO(em);
-		Pedido pedido = new Pedido(cliente);
-		pedido.adicionarItem(new ItemPedido(2, pedido, produto));
+		Pedido pedido1 = new Pedido(cliente);
+		pedido1.adicionarItem(new ItemPedido(2, pedido1, produto1));
+		Pedido pedido2 = new Pedido(cliente);
+		pedido2.adicionarItem(new ItemPedido(5, pedido2, produto2));
+		Pedido pedido3 = new Pedido(cliente);
+		pedido3.adicionarItem(new ItemPedido(10, pedido3, produto3));
 		
 		em.getTransaction().begin();
-		pedidoDAO.cadastrar(pedido);
+		pedidoDAO.cadastrar(pedido1);
+		pedidoDAO.cadastrar(pedido2);
+		pedidoDAO.cadastrar(pedido3);
 		
 		em.getTransaction().commit();
 		
-		Pedido pedidoCadastrado = pedidoDAO.buscarPorId(1l);
-		System.out.println(pedidoCadastrado);
+		BigDecimal valorTotalPedidos = pedidoDAO.somaValorTotalPedidos();
+		System.out.println("Valor total de pedidos: " + valorTotalPedidos);
+		
+		List<RelatorioDeVendasVo> relatorio = pedidoDAO.getRelatorioVendas();
+		relatorio.forEach(r -> System.out.println(r));
+				
 		em.close();
 		
 	}
