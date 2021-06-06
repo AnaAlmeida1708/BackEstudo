2da45049b2363d2306b479fdd801e3b0f8ce4eda
anaalmeida1708 <anacarolinama@gmail.com>
2021-06-06 11:10:26 -0300

    Criando projeto - Design Patterns I

---
+++ Alura/Design Patterns/loja/src/br/com/alura/loja/imposto/TestesImpostos.java
@@ -0,0 +1,16 @@
+package br.com.alura.loja.imposto;
+
+import java.math.BigDecimal;
+
+import br.com.alura.loja.orcamento.Orcamento;
+
+public class TestesImpostos {
+
+	public static void main(String[] args) {
+		Orcamento orcamento = new Orcamento(new BigDecimal("1000"));
+		CalculadoraDeImposto calculadora = new CalculadoraDeImposto();
+		System.out.println(calculadora.calculaImposto(orcamento, TipoImposto.ISS));
+
+	}
+
+}
