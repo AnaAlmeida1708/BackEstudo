a6dd2ce905ba15e88b85af5f5e081f28e63de03f
anaalmeida1708 <anacarolinama@gmail.com>
2021-06-09 09:38:29 -0300

    State - Design Patterns I

---
+++ Alura/Design Patterns/loja/src/br/com/alura/loja/orcamento/situacao/Reprovado.java
@@ -0,0 +1,12 @@
+package br.com.alura.loja.orcamento.situacao;
+
+import br.com.alura.loja.orcamento.Orcamento;
+
+public class Reprovado extends Situacao {
+	
+	@Override
+	public void finalizar(Orcamento orcamento) {
+		orcamento.setSituacao(new Finalizado());
+	}
+
+}
