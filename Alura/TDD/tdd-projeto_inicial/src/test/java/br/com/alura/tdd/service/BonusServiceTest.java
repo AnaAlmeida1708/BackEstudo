f3be0ede2f4131453dacd934973936c4b843e953
anaalmeida1708 <anacarolinama@gmail.com>
2021-07-25 08:44:37 -0300

    Trabalhando com exceptions

--- Alura/TDD/tdd-projeto_inicial/src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ Alura/TDD/tdd-projeto_inicial/src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -1,10 +1,9 @@
 package br.com.alura.tdd.service;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import br.com.alura.tdd.modelo.Funcionario;
@@ -14,9 +13,18 @@ class BonusServiceTest {
 	@Test
 	void bonusDeveSerZeroParaFuncionarioComSalarioMuitoAlto() {
 		BonusService service = new BonusService();
-		BigDecimal bonus = service.calcularBonus(new Funcionario("Ana", LocalDate.now(), new BigDecimal("25000")));
 		
-		assertEquals(new BigDecimal("0.00"), bonus);
+		Assertions.assertThrows(IllegalArgumentException.class, 
+				() -> service.calcularBonus(new Funcionario("Ana", LocalDate.now(), new BigDecimal("25000"))));
+		
+		//Geralmente usado quando se quer validar além da exception a mensagem da exception
+//		try {
+//			service.calcularBonus(new Funcionario("Ana", LocalDate.now(), new BigDecimal("25000")));
+//			Assertions.fail("Não deu a exception");
+//		} catch (Exception e) {
+//			Assertions.assertEquals("Funcionário não pode receber bônus, pois valor bônus seria superior a R$ 1000,00", e.getMessage());
+//		}
+
 	}
 	
 	@Test
@@ -24,7 +32,7 @@ class BonusServiceTest {
 		BonusService service = new BonusService();
 		BigDecimal bonus = service.calcularBonus(new Funcionario("Ana", LocalDate.now(), new BigDecimal("4500")));
 		
-		assertEquals(new BigDecimal("450.00"), bonus);
+		Assertions.assertEquals(new BigDecimal("450.00"), bonus);
 	}
 	
 	@Test
@@ -32,7 +40,7 @@ class BonusServiceTest {
 		BonusService service = new BonusService();
 		BigDecimal bonus = service.calcularBonus(new Funcionario("Ana", LocalDate.now(), new BigDecimal("10000")));
 		
-		assertEquals(new BigDecimal("1000.00"), bonus);
+		Assertions.assertEquals(new BigDecimal("1000.00"), bonus);
 	}
 
 }
