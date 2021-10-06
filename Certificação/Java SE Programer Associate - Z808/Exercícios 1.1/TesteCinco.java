864f1b9393aee164190889b977a9c4cad2e3f287
Ana Carolina <anacarolinama@gmail.com>
2021-10-06 20:14:39 -0300

    Certificação 808 - Atividades cap.1

---
+++ Certificação/Java SE Programer Associate - Z808/Exercícios 1.1/TesteCinco.java
@@ -0,0 +1,10 @@
+class TesteCinco	{
+	static int i = 3;
+	
+	public	static	void main(String[] a) {
+		for (new TesteCinco().i = 10; new TesteCinco().i < 100;
+		new TesteCinco().i++){
+			System.out.println(i);
+		}
+	}
+}
---
+++ Certificação/Java SE Programer Associate - Z808/Exercícios 1.1/TesteTres.java
@@ -0,0 +1,9 @@
+class TesteTres	{
+public	static	void main(String[]	args) {
+	for	(int i=0; i	< 20; i++){
+		System.out.println(i);
+	}
+	int i = 15;
+	System.out.println(i);
+	}
+}
